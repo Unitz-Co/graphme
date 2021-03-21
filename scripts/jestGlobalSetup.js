a9ad0cea8e93734c02e027eefb4c7fe49c76954e
SiTruong-TR7 <si.truongtan@gmail.com>
2021-03-21 11:00:00 +0700

    rollback testing graphql endpoint

--- scripts/jestGlobalSetup.js
+++ scripts/jestGlobalSetup.js
@@ -1,7 +1,8 @@
 module.exports = () => {
   // require('dotenv').config();
       // config vars
-  global.process.env.HASURA_GRAPHQL_ENDPOINT = 'https://mock-graphme.herokuapp.com/v1/graphql';
+  global.process.env.HASURA_GRAPHQL_ENDPOINT = 'http://localhost:28080/v1/graphql';
+  // global.process.env.HASURA_GRAPHQL_ENDPOINT = 'https://mock-graphme.herokuapp.com/v1/graphql';
   global.process.env.HASURA_GRAPHQL_ADMIN_SECRET = 'admin';
   
 };
